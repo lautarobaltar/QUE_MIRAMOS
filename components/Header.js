ed098d5b5af026f268de92fb20154de706445868
augustotoloza <toloza.augusto@gmail.com>
2020-11-17 13:45:59 -0300

    asset stuff

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from "./styles/index"
 import { View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 function Header() {
     return (
         <View style={styles.header}  >
-            <Ionicons name="ios-settings" size={32} color="grey" onPress={() => console.log("home settings")}/>
-            <Ionicons name="ios-code-working" size={50} color="black" onPress={() => console.log("home logo")}/>
+            <Ionicons name="ios-settings" size={32} color="black" onPress={() => console.log("home settings")}/>
+            <FontAwesome name="eye" size={50} color="black" onPress={() => console.log("home logo")}/>
             <Ionicons name="md-chatboxes" size={32} color="black" onPress={() => console.log("home chats")}/>
         </View>
     )

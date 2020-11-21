6ba524ab9e878195b29547a2c1ce404bb3cacc0a
augustotoloza <toloza.augusto@gmail.com>
2020-11-21 02:45:06 -0300

    chat added

--- views/Chat.js
+++ views/Chat.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 import styles from "../components/styles/index"
 import { SafeAreaView, Text, View } from 'react-native';
 import Header from "../components/Header"
@@ -6,14 +7,31 @@ import Cards from "../components/Cards"
 import Buttons from "../components/Buttons"
 
 export default function Chat() {
+  const [messages, setMessages] = useState([]);
+  useState(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello bitches',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: '¿Qué miramos?',
+          avatar: 'https://placeimg.com/140/140/any'
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
-    <SafeAreaView style={styles.container}>
-      
-      <Text>
-
-      Chat view
-      </Text>
-
-    </SafeAreaView>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{_id: 1}}
+    />
   );
 }

191d3c83fa9c8634bdd6da8d9b5f3d0ba3c585e5
augustotoloza <toloza.augusto@gmail.com>
2020-11-24 14:19:43 -0300

    modal

--- components/styles/index.js
+++ components/styles/index.js
@@ -33,5 +33,49 @@ export default StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-around"
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        
+        width: '80%',
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        display: 'flex'
+      },
+      openButton: {
+        backgroundColor: "#F194FF",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+      },
+      modalText: {
+        marginBottom: 0,
+        textAlign: "center",
+        fontFamily: "OpenSansCondensed_700Bold",
+        textTransform: "uppercase",
+        letterSpacing: -0.6,
+        fontSize: 40,
+        lineHeight: 44
+      }
 });
